a6771e6051f921445aaa016738828b2974aeaec8
neutron <chosh901@naver.com>
2023-05-17 08:47:30 +0900

    분할 캡처 및 전자책 최적 사이즈 테스트 코드 추가

--- capture_macro/capture_macro.py
+++ capture_macro/capture_macro.py
@@ -6,9 +6,10 @@ import PyPDF2
 from PIL import Image
 import time
 import os
-from reportlab.lib.pagesizes import letter
+from reportlab.lib.pagesizes import letter,LEDGER,ELEVENSEVENTEEN
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
+
 
 my_dir = "./book/"
 img_dir = f'{my_dir}img/'
@@ -16,75 +17,88 @@ book_name = "토비의스프링1"
 
 
 # Define the top-left and bottom-right coordinates of the region to capture
-left, top, right, bottom = 550, 90, 1350, 1070
+left, top, right, bottom = 190-30, 40, 1660+60, 1020
 width, height = right-left, bottom - top
-right, bottom = left + width, top + height
-
-screenshot = pyautogui.screenshot(region=(left, top, width, height))
-screenshot = np.array(screenshot)
-screenshot = screenshot[top:top+height, left:left+width, :]
-
-# Convert the screenshot to grayscale and apply thresholding
-gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
-thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-
-# Perform OCR on the thresholded image
-text = pytesseract.image_to_string(thresh)
-
-# Get the coordinates of the bounding box around the text
-boxes = pytesseract.image_to_boxes(thresh)
-coords = boxes.split('\n')
-x1, y1, x2, y2 = map(int, coords[0].split()[1:5])
-
-# Calculate the coordinates of the rectangular part of the screen around the text
-text_area_left = left + x1
-text_area_top = top + (height - y2)
-text_area_width = x2 - x1
-text_area_height = y2
-
-# Get the rectangular part of the screen around the text
-text_area_image = pyautogui.screenshot(region=(text_area_left, text_area_top, text_area_width, text_area_height))
-
-# Save the image
-text_area_image.save(f'{img_dir}/text_area.png')
-
-
+# right, bottom = left + width, top + height
 
 # Define the coordinates of the point to click
-x, y = 1866, 510
+btn_x, btn_y = 1865, 510
 
+def move_click_scroll(x, y):
+    pyautogui.moveTo(x, y)
+    time.sleep(0.5)
+    pyautogui.click(x, y)
+    time.sleep(0.5)
+    pyautogui.moveTo(x-100, y)
+    time.sleep(0.5)
+    pyautogui.scroll(-100)
+    time.sleep(0.5)
+
+def screenshot_save(page:int, area:tuple):
+    screenshot = pyautogui.screenshot(region=area)
+    pil_image = Image.frombytes("RGB", screenshot.size, screenshot.tobytes())
+    pil_image.save(f'{img_dir}{page:03d}.png')
+
+
+# -30, 60
+def find_best_point(page:int):
+    test_cases = [-30, -20, -10, -5, 0, 5, 10, 20, 30,40,50, 60]
+    for t1 in test_cases:
+        left, top = 190 -30, 40
+        for t2 in test_cases:
+            right, bottom = 1660+t2, 1020
+            width, height = right-left, bottom - top
+
+            left_area = (left, top, width/2, height)
+            screenshot_save(page, left_area)
+
+            right_area = (left+width/2, top, width/2, height)
+            screenshot_save(page+1, right_area)
+
+            page+=2
+            move_click_scroll(btn_x, btn_y)
+
+
+
+
+
+time.sleep(1)
+
+find_best_point(45)
 
 # Save the captured image to a file
-# page = 20
-# while page <= 878:
-#     time.sleep(1)
-#     # Capture the screen region as a PIL image
-#     screenshot = pyautogui.screenshot(region=(left, top, width, height))
-#     pil_image = Image.frombytes("RGB", screenshot.size, screenshot.tobytes())
-#     pil_image.save(f'{img_dir}{page:03d}.png')
+page = 19
+while page <= 40: #878:
+    # Capture the screen region as a PIL image
+    left_area = (left, top, width/2, height)
+    screenshot_save(page, left_area)
 
-#     # pyautogui.moveTo(x, y)
-#     pyautogui.click(x, y)
-#     page+=1
+    right_area = (left+width/2, top, width/2, height)
+    screenshot_save(page+1, right_area)
 
+    page+=2
+    move_click_scroll(btn_x, btn_y)
 
+    
+    
+pdf_writer = PyPDF2.PdfWriter()
 
+image_list = os.listdir(img_dir)
+for image_file in image_list:
+    img = ImageReader(f'{img_dir}{image_file}')
 
-# pdf_writer = PyPDF2.PdfWriter()
+    pdf_canvas = canvas.Canvas(f'{my_dir}temp.pdf', pagesize=letter)
+    pdf_canvas.drawImage(img, x=0, y=0, width=letter[0], height=letter[1])
+    pdf_canvas.save()
 
+    pdf_temp = open(f'{my_dir}temp.pdf', 'rb')
+    pdf_temp_reader = PyPDF2.PdfReader(pdf_temp)
+    pdf_writer.add_page(pdf_temp_reader.pages[0])
+    pdf_temp.close()
+    
+    
 
-# image_list = os.listdir(img_dir)
-# for image_file in image_list:
-#     image = ImageReader(f'{img_dir}{image_file}')
-#     pdf_canvas = canvas.Canvas(f'{my_dir}temp.pdf', pagesize=letter)
-#     pdf_canvas.drawImage(image, x=0, y=0, width=letter[0], height=letter[1])
-#     pdf_canvas.save()
+with open(f'{my_dir}{book_name}.pdf', 'wb') as output_file:
+    pdf_writer.write(output_file)
 
-#     pdf_temp = open(f'{my_dir}temp.pdf', 'rb')
-#     pdf_temp_reader = PyPDF2.PdfReader(pdf_temp)
-#     pdf_writer.add_page(pdf_temp_reader.pages[0])
-#     pdf_temp.close()
-
-# with open(f'{my_dir}{book_name}', 'wb') as output_file:
-#     pdf_writer.write(output_file)
 
