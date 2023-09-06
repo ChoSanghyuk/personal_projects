f84af18b0ac52cfa3584870d1f9f40507ea60396
cnssh <chosh901@naver.com>
2023-09-06 12:53:59 +0900

    String 날짜변환 샘플코드

--- String_Date_Convert/src/StringToDate.java
+++ String_Date_Convert/src/StringToDate.java
@@ -5,14 +5,14 @@ import java.text.SimpleDateFormat;
 public class StringToDate {
     public static void main(String[] args) throws ParseException {
 
-        String sampleDate = "2023/09/05 23:22:11";
+        String dateString = "2023/09/05 23:22:11";
 
         // 입력될 문자열의 포맷을 지정
         SimpleDateFormat parser = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         // 결과로 도출될 문자열의 포맷을 지정
         SimpleDateFormat output = new SimpleDateFormat("yyyy-MM-dd");
         // parse : 문자열 => java.util.Date  format : java.util.Date => 문자열
-        java.sql.Date date = Date.valueOf(output.format(parser.parse(sampleDate)));
+        java.sql.Date date = Date.valueOf(output.format(parser.parse(dateString)));
 
         System.out.println(date.toString()); // 2023-09-05
 
--- String_Date_Convert/src/StringToTimestamp.java
+++ String_Date_Convert/src/StringToTimestamp.java
@@ -7,16 +7,16 @@ import java.time.LocalDateTime;
 public class StringToTimestamp {
     public static void main(String[] args) throws ParseException {
 
-        String sampleDate = "2023/09/05 23:22:11.123123";
+        String dateString = "2023/09/05 23:22:11.123123";
 
         // 입력될 문자열의 포맷을 지정
         SimpleDateFormat parser = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSSD");
         // 결과로 도출될 문자열의 포맷을 지정
         SimpleDateFormat output = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSD");
         // parse : 문자열 => java.util.Date  format : java.util.Date => 문자열
-        Timestamp date = Timestamp.valueOf(output.format(parser.parse(sampleDate)));
+        Timestamp timestamp = Timestamp.valueOf(output.format(parser.parse(dateString)));
 
-        System.out.println(date.toString()); // 2023-09-05
+        System.out.println(timestamp.toString()); // 2023-09-05
 
     }
 }
