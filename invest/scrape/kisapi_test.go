53d4debba8192d5d1525009471dad63abcc3d9c8
cnssh <chosh901@naver.com>
2024-10-04 13:02:51 +0900

    .

--- invest/scrape/kisapi_test.go
+++ invest/scrape/kisapi_test.go
@@ -15,7 +15,6 @@ func TestKis(t *testing.T) {
 	s := NewScraper(
 		conf,
 		WithKIS(conf.KisAppKey(), conf.KisAppSecret()),
-		WithToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjAyYjI2MThkLTVkMjgtNDE1Ni1hMGVkLTZjODQxZGRiYmVhMyIsInByZHRfY2QiOiIiLCJpc3MiOiJ1bm9ndyIsImV4cCI6MTcyODA4NDEzMCwiaWF0IjoxNzI3OTk3NzMwLCJqdGkiOiJQU2c0QjA4aFFDdzlqTUVvVmRYdGw1b2o1REhJaUpkY2lCZnMifQ.EFeGpl2PJ8md5f7Tgvj9Vw4QysaNpv1EmdwPeqcXD0gcroTc5nVMnAuBUqfeL3kld4HVuVLUEZoj66xiuw7nJg"),
 	)
 
 	t.Run("Token Generate", func(t *testing.T) {
