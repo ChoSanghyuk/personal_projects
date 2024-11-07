5f8b6404013ad1a7bfd0871b77cace6b44c9c9ce
cnssh <chosh901@naver.com>
2024-11-07 23:08:42 +0900

    script modify

--- auto_deploy/deploy_lolche_bot.sh
+++ auto_deploy/deploy_lolche_bot.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-cd ~/workspace/invest_indicator
+cd ~/workspace/lolche_bot
 git pull origin main
-/home/cho/go/bin/go build -o invest_indicator
+/home/cho/go/bin/go build -o lolche_bot
 PID=$(cat pidfile.txt 2>/dev/null)
 if kill -0 $PID 2>/dev/null; then echo "Process $PID is running. Kill process"; kill $PID; fi
-./invest_indicator &
+./lolche_bot &
 echo $! > pidfile.txt
 
