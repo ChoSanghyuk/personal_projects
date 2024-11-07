6bfdd50c619ddfeb9a2eb8b3c312cbb0a1ab7b24
cnssh <chosh901@naver.com>
2024-11-07 22:59:51 +0900

    script

--- auto_deploy/deploy_invest_indicator.sh
+++ auto_deploy/deploy_invest_indicator.sh
@@ -1,1 +1,9 @@
-echo "HELLO"
+#!/bin/sh
+cd ~/workspace/invest_indicator
+git pull origin master
+/home/cho/go/bin/go build -o invest_indicator
+PID=$(cat pidfile.txt 2>/dev/null)
+if kill -0 $PID 2>/dev/null; then echo "Process $PID is running. Kill process"; kill $PID; fi
+./invest_indicator &
+echo $! > pidfile.txt
+
