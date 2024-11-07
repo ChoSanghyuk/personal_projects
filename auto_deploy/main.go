9675c9f84fc68a982e050e0e9ffb28fdc1c78f95
cnssh <chosh901@naver.com>
2024-11-07 23:01:12 +0900

    GET => POST

--- auto_deploy/main.go
+++ auto_deploy/main.go
@@ -10,14 +10,14 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/invest_indicator", func(c *fiber.Ctx) error {
+	app.Post("/invest_indicator", func(c *fiber.Ctx) error {
 		cmd := exec.Command("./deploy_invest_indicator.sh")
 		if err := cmd.Run(); err != nil {
 			return c.SendString(err.Error())
 		}
 		return nil
 	})
-	app.Get("/lolche_bot", func(c *fiber.Ctx) error {
+	app.Post("/lolche_bot", func(c *fiber.Ctx) error {
 		cmd := exec.Command("./deploy_lolche_bot.sh")
 		if err := cmd.Run(); err != nil {
 			return c.SendString(err.Error())
