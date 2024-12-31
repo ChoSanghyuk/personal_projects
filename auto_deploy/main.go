81aff351f7ba681414708496b3d2ce0784b80c38
chosh901 <chosh901@naver.com>
2024-12-31 10:57:52 +0900

    [modify] change file name

--- auto_deploy/main.go
+++ auto_deploy/main.go
@@ -11,21 +11,21 @@ func main() {
 	app := fiber.New()
 
 	app.Post("/invest_indicator", func(c *fiber.Ctx) error {
-		cmd := exec.Command("./deploy_invest_indicator.sh")
+		cmd := exec.Command("./auto_invest_indicator.sh")
 		if err := cmd.Run(); err != nil {
 			return c.SendString(err.Error())
 		}
 		return nil
 	})
 	app.Post("/lolche_bot", func(c *fiber.Ctx) error {
-		cmd := exec.Command("./deploy_lolche_bot.sh")
+		cmd := exec.Command("./auto_lolche_bot.sh")
 		if err := cmd.Run(); err != nil {
 			return c.SendString(err.Error())
 		}
 		return nil
 	})
 	app.Post("/lolche_bot_rust", func(c *fiber.Ctx) error {
-		cmd := exec.Command("./deploy_lolche_bot_rust.sh")
+		cmd := exec.Command("./auto_lolche_bot_rust.sh")
 		if err := cmd.Run(); err != nil {
 			return c.SendString(err.Error())
 		}
