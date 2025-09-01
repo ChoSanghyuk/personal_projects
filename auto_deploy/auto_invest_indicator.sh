f590eb7bc4d1e47637dd65581403b1d7ae301708
ChoSanghyuk <59812791+ChoSanghyuk@users.noreply.github.com>
2025-09-01 10:19:22 +0900

    Update auto_invest_indicator.sh

--- auto_deploy/auto_invest_indicator.sh
+++ auto_deploy/auto_invest_indicator.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd ~/workspace/invest_indicator
 git pull origin main
-/home/cho/go/bin/go build -o invest_indicator
+# /home/cho/go/bin/go build -o invest_indicator ./cmd/
+docker run --rm -v $(pwd):/app -w /app golang:1.24 go build -o investindicator cmd/main.go
 PID=$(cat pidfile.txt 2>/dev/null)
 if kill -0 $PID 2>/dev/null; then echo "Process $PID is running. Kill process"; kill $PID; fi
-./invest_indicator &
+nohup ./invest_indicator > /var/log/invest/$(date +%F).log 2>&1 &
 echo $! > pidfile.txt
-
